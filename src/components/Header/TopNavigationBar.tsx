734768d55ef65d6415bef743d9a1100429df6fa6
Kevin Kamto <kamtokevin@gmail.com>
2024-06-11 05:34:33 -0400

    feat: basic top navigational bar implementation

---
+++ src/components/Header/TopNavigationBar.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+
+const TopNavigationBar: React.FC = () => {
+    
+    return (
+        <div>
+            This is the top navigation bar
+        </div>
+    )
+
+}
+
+export default TopNavigationBar
