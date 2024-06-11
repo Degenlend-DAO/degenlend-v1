8c4a58b7deaf9f6543eb184292926b1aed1ad97c
Kevin Kamto <kamtokevin@gmail.com>
2024-06-11 05:48:21 -0400

    feat: basic markets implementation

---
+++ src/components/Dashboard/markets.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const markets: React.FC = () => {
+
+    return (
+        <div>
+            These are the markets
+        </div>
+    )
+}
+
+
+export default markets
