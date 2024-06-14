6db47e4cb8bbb6df789e8fe62871331be7d7aa3f
Kevin Kamto <kamtokevin@gmail.com>
2024-06-13 23:10:22 -0400

    chore(WIP): Top Wallet Card placeholder

---
+++ src/components/Header/TopWalletCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function TopWalletCard() {
+    return (
+        <div>
+            Top Wallet Card goes here
+        </div>
+    )
+}
+
+export default TopWalletCard;
