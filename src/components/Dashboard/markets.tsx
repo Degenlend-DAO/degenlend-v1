f4c8b27e77911739197a88e031ee770d70073743
Kevin Kamto <kamtokevin@gmail.com>
2024-06-13 23:09:44 -0400

    chore(markets): WIP Markes implementation

--- src/components/Dashboard/markets.tsx
+++ src/components/Dashboard/markets.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
+import Link from '@mui/material/Link';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import { Title } from '@mui/icons-material';
+
+// Generate Markets Data
+function createData (
+    id: number,
+    name: string,
+    tokenString: string,
+    amount: number
+) {
+    return {
+        id,
+        name,
+        tokenString,
+        amount
+    }
+};
 
 
-const markets: React.FC = () => {
-
+function markets() {
     return (
-        <div>
-            These are the markets
-        </div>
+        <React.Fragment>
+            <Title> Markets </Title>
+            <Table size='medium'>
+            </Table>
+        </React.Fragment>
     )
 }
 
