8d184be20a6c8c3ac878f159b1a06cc464ea4684
Kevin Kamto <kamtokevin@gmail.com>
2024-06-17 22:51:42 -0400

    feat: replaced basic root with UI

--- src/views/root.tsx
+++ src/views/root.tsx
@@ -1,14 +1,29 @@
+import { Box, PaletteMode } from '@mui/material';
 import React from 'react'
 
+// Design Library
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import TopNavigationBar from '../components/Header/TopNavigationBar';
 
 
-const Root: React.FC = () => <>
-<div>
-    <h2>
-        This is the root
-    </h2>
-</div>
-</>
+function Root() {
+    const [mode, setMode] = React.useState<PaletteMode>('light');
+    const defaultTheme = createTheme({ palette: { mode } });
 
+    const toggleColorMode = () => {
+        setMode((prev: string) => (prev === 'dark' ? 'light' : 'dark'));
+      };
+
+      return (
+        <ThemeProvider theme={defaultTheme}>
+            <CssBaseline />
+            <TopNavigationBar mode={mode} toggleColorMode={toggleColorMode} />
+                <Box sx={{ bgcolor: 'background.default' }}>
+                    
+                </Box>
+        </ThemeProvider>
+      );
+}
 
 export default Root;
