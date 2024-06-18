47e5734555f52c0ede93e685db51b9cc33acdafa
rbharath99 <dbossman60@gmail.com>
2024-06-18 13:51:27 -0400

    feat: routing setup

--- src/components/Header/TopNavigationBar.tsx
+++ src/components/Header/TopNavigationBar.tsx
@@ -12,50 +12,51 @@ import Drawer from '@mui/material/Drawer';
 import MenuIcon from '@mui/icons-material/Menu';
 
 import ToggleColorMode from '../../utils/ToggleColorMode'
+import { Link } from 'react-router-dom';
 
-    const logoStyle = {
-        width: '140px',
-        height: 'auto',
-        cursor: 'pointer',
-    }
+const logoStyle = {
+    width: '140px',
+    height: 'auto',
+    cursor: 'pointer',
+}
 
-    interface AppBarProps {
-        mode: PaletteMode;
-        toggleColorMode: () => void;
-    }
+interface AppBarProps {
+    mode: PaletteMode;
+    toggleColorMode: () => void;
+}
 
-function TopNavigationBar({mode, toggleColorMode}: AppBarProps) {
+function TopNavigationBar({ mode, toggleColorMode }: AppBarProps) {
 
     const [open, setOpen] = React.useState(false);
 
     const toggleDrawer = (newOpen: boolean) => () => {
-      setOpen(newOpen);
+        setOpen(newOpen);
     };
 
     const scrollToSection = (sectionId: string) => {
         const sectionElement = document.getElementById(sectionId);
         const offset = 128;
         if (sectionElement) {
-          const targetScroll = sectionElement.offsetTop - offset;
-          sectionElement.scrollIntoView({ behavior: 'smooth' });
-          window.scrollTo({
-            top: targetScroll,
-            behavior: 'smooth',
-          });
-          setOpen(false);
+            const targetScroll = sectionElement.offsetTop - offset;
+            sectionElement.scrollIntoView({ behavior: 'smooth' });
+            window.scrollTo({
+                top: targetScroll,
+                behavior: 'smooth',
+            });
+            setOpen(false);
         }
-      };
+    };
 
     return (
         <div>
             <AppBar
-            position='fixed'
-            sx={{
-                boxShadow: 0,
-                bgcolor: 'transparent',
-                backgroundImage: 'none',
-                mt: 2,
-            }}
+                position='fixed'
+                sx={{
+                    boxShadow: 0,
+                    bgcolor: 'transparent',
+                    backgroundImage: 'none',
+                    mt: 2,
+                }}
             >
                 <Container maxWidth='lg'>
                     <Toolbar
@@ -67,62 +68,62 @@ function TopNavigationBar({mode, toggleColorMode}: AppBarProps) {
                             borderRadius: '999px',
                             bgcolor:
                                 theme.palette.mode === 'light'
-                                ? 'rgba(255, 255, 255, 0.4)'
-                                : 'rgba(0, 0, 0, 0.4)',
+                                    ? 'rgba(255, 255, 255, 0.4)'
+                                    : 'rgba(0, 0, 0, 0.4)',
                             backdropFilter: 'blur(24px)',
                             maxHeight: 40,
                             border: '1px solid',
                             borderColor: 'divider',
                             boxShadow:
                                 theme.palette.mode === 'light'
-                                ? `0 0 1px rgba(85, 166, 246, 0.1), 1px 1.5px 2px -1px rgba(85, 166, 246, 0.15), 4px 4px 12px -2.5px rgba(85, 166, 246, 0.15)`
-                                : '0 0 1px rgba(2, 31, 59, 0.7), 1px 1.5px 2px -1px rgba(2, 31, 59, 0.65), 4px 4px 12px -2.5px rgba(2, 31, 59, 0.65)',
-                            })}
+                                    ? `0 0 1px rgba(85, 166, 246, 0.1), 1px 1.5px 2px -1px rgba(85, 166, 246, 0.15), 4px 4px 12px -2.5px rgba(85, 166, 246, 0.15)`
+                                    : '0 0 1px rgba(2, 31, 59, 0.7), 1px 1.5px 2px -1px rgba(2, 31, 59, 0.65), 4px 4px 12px -2.5px rgba(2, 31, 59, 0.65)',
+                        })}
                     >
-                    <Box
-                    sx={{
-                    flexGrow: 1,
-                    display: 'flex',
-                    alignItems: 'center',
-                    ml: '-18px',
-                    px: 0,
-                    }}
-                    >
-                        <img src='../../assets/img/degenlend-variation2.png' style={logoStyle} alt='Degenlend logo'/>
+                        <Box
+                            sx={{
+                                flexGrow: 1,
+                                display: 'flex',
+                                alignItems: 'center',
+                                ml: '-18px',
+                                px: 0,
+                            }}
+                        >
+                            <img src='../../assets/img/degenlend-variation2.png' style={logoStyle} alt='Degenlend logo' />
 
-                            <Box sx={{display: {xs: 'none', md: 'flex'}}}>
+                            <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
                                 {/* Supplying & Borrowing Activities on The market */}
                                 <MenuItem
-                                onClick={() => scrollToSection('Markets')}
-                                sx={{ py: '6px', px: '12px' }}
+                                    onClick={() => scrollToSection('Markets')}
+                                    sx={{ py: '6px', px: '12px' }}
                                 >
-                                <Typography variant="body2" color="text.primary">
-                                    Markets
-                                </Typography>
+                                    <Typography variant="body2" color="text.primary">
+                                        <Link to="/markets">Markets</Link>
+                                    </Typography>
                                 </MenuItem>
 
                                 {/* Turn your WSX into liquistake WSX */}
                                 <MenuItem>
                                     <Typography variant='body2' color='text.primary'>
-                                        Stake WSX
+                                        <Link to="/stake">Stake WSX</Link>
                                     </Typography>
                                 </MenuItem>
 
                                 {/* FAQ for how DegenLend works, and what to expect */}
                                 <MenuItem>
                                     <Typography variant="body2" color="text.primary">
-                                        FAQ
+                                        <Link to="/faq">FAQ</Link>
                                     </Typography>
                                 </MenuItem>
                                 <MenuItem>
                                     <Typography variant='body2' color='text.primary'>
-                                        Vote
+                                        <Link to="/vote">Vote</Link>
                                     </Typography>
                                 </MenuItem>
-                            </Box>   
+                            </Box>
                             <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'end', flexGrow: 1 }}>
-                            <ToggleColorMode mode={mode} toggleColorMode={toggleColorMode} />
-                            </Box>                         
+                                <ToggleColorMode mode={mode} toggleColorMode={toggleColorMode} />
+                            </Box>
                         </Box>
                     </Toolbar>
                 </Container>
--- src/components/routes/routes.tsx
+++ src/components/routes/routes.tsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import Root from '../../views/root';
 
-const RoutesComponent =() => {
+const RoutesComponent = () => {
     return (
         <Routes>
             <Route path='/' element={<Root />} />
+            <Route path='/markets' element={<></>} />
+            <Route path='/stake' element={<></>} />
+            <Route path='/faq' element={<></>} />
+            <Route path='/vote' element={<></>} />
         </Routes>
     );
 }
