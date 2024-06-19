a384319e65ce17b14528b8ecf305833e7bb40c7c
rbharath99 <dbossman60@gmail.com>
2024-06-19 14:14:39 -0400

    chore: initial layout on governance page. Added overview and recent proposals

--- src/components/Governance/Governance.tsx
+++ src/components/Governance/Governance.tsx
@@ -1,9 +1,75 @@
-import { Typography } from "@mui/material";
+import { Box, Container, Grid, Paper, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Governance = () => {
+    const data = [
+        { title: 'COMP Remaining', value: '0', linkText: 'VIEW' },
+        { title: 'Votes Delegated', value: '0' },
+        { title: 'Voting Addresses', value: '0' }
+    ];
+
+    const proposals = [
+        { title: '[Gauntlet] Optimism USDC Risk Recommendations - 06-17-24', status: 'Review', time: '1 day, 6 hrs until voting' },
+        { title: 'Initialize cWETHv3 on Arbitrum', status: 'Active', time: '11 hrs, 24 mins left' },
+        { title: 'Initialize cUSDTv3 on Optimism', status: 'Passed', time: 'Queued June 19th, 2024' }
+    ];
+
     return (
         <>
-            <Typography>Governance</Typography>
+            <Container maxWidth="lg">
+                <Box mt={4}>
+                    <Grid container spacing={4} direction={'column'}>
+                        <Typography align="left">Governance Overview</Typography>
+                        <Grid container spacing={2}>
+                            {data.map((item, index) => (
+                                <Grid item xs={12} sm={4} key={index}>
+                                    <Paper elevation={3} style={{ padding: '20px', backgroundColor: '#1E1E1E', color: '#FFF' }}>
+                                        <Typography variant="h6" gutterBottom>
+                                            {item.title}
+                                        </Typography>
+                                        <Typography variant="h4" gutterBottom>
+                                            {item.value}
+                                        </Typography>
+                                        {item.linkText && (
+                                            <Box display="flex" alignItems="center">
+                                                <Typography variant="body2" style={{ color: '#66BB6A' }}>
+                                                    <Link to='/governance/comp'>{item.linkText}</Link>
+                                                </Typography>
+                                                <Box width="100%" ml={1}>
+                                                    <Box
+                                                        bgcolor="#66BB6A"
+                                                        height={4}
+                                                        width="50%"
+                                                        borderRadius={2}
+                                                    />
+                                                </Box>
+                                            </Box>
+                                        )}
+                                    </Paper>
+                                </Grid>
+                            ))}
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Paper elevation={3} style={{ padding: '20px' }}>
+                                <Typography variant="h6">Recent Proposals</Typography>
+                                {proposals.map((proposal, index) => (
+                                    <Link to={`/governance/comp/${index}`} style={{ textDecoration: 'none', color: 'inherit' }} key={index}>
+                                        <Box my={2}>
+                                            <Typography variant="subtitle1">{proposal.title}</Typography>
+                                            <Typography variant="body2" color="textSecondary">{proposal.status} • {proposal.time}</Typography>
+                                            <Box display="flex" justifyContent="space-between" mt={1}>
+                                                <Typography variant="body2">For 100%</Typography>
+                                                <Typography variant="body2">Against 0%</Typography>
+                                                <Typography variant="body2">Abstain 0%</Typography>
+                                            </Box>
+                                        </Box>
+                                    </Link>
+                                ))}
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Container>
         </>
     );
 }
--- src/components/routes/routes.tsx
+++ src/components/routes/routes.tsx
@@ -2,13 +2,14 @@ import { Routes, Route } from 'react-router-dom'
 import Root from '../../views/root';
 import FrequentlyAskedQuestions from '../FrequentlyAskedQuestions/FrequentlyAskedQuestions';
 import Vote from '../Vote/Vote';
+import Governance from '../Governance/Governance';
 
 const RoutesComponent = () => {
     return (
         <Routes>
             <Route path='/' element={<Root />} />
             <Route path='/markets' element={<></>} />
-            <Route path='/governance' element={<></>} />
+            <Route path='/governance' element={<Governance />} />
             <Route path='/stake' element={<></>} />
             <Route path='/faq' element={<FrequentlyAskedQuestions />} />
             <Route path='/vote' element={<Vote />} />
