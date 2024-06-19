979cf871b7197bd67990f94f42902ec83b7e21fa
rbharath99 <dbossman60@gmail.com>
2024-06-19 00:43:53 -0400

    chore: setup Governance page

---
+++ src/components/Governance/Governance.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+const Governance = () => {
+    return (
+        <>
+            <Typography>Governance</Typography>
+        </>
+    );
+}
+
+export default Governance;
