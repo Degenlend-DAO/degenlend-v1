84702b91b020e001784cd03581d76a4ac706273b
rbharath99 <dbossman60@gmail.com>
2024-06-18 23:47:27 -0400

    feat: FAQ page

--- src/components/routes/routes.tsx
+++ src/components/routes/routes.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import Root from '../../views/root';
+import FrequentlyAskedQuestions from '../FrequentlyAskedQuestions/FrequentlyAskedQuestions';
 
 const RoutesComponent = () => {
     return (
@@ -7,7 +8,7 @@ const RoutesComponent = () => {
             <Route path='/' element={<Root />} />
             <Route path='/markets' element={<></>} />
             <Route path='/stake' element={<></>} />
-            <Route path='/faq' element={<></>} />
+            <Route path='/faq' element={<FrequentlyAskedQuestions />} />
             <Route path='/vote' element={<></>} />
         </Routes>
     );
