2004bc9a0207d9fffdf0bacc980b8de1288f40c6
Kevin Kamto <kamtokevin@gmail.com>
2024-06-19 13:08:04 -0400

    feat(WIP): Connect Wallet

---
+++ src/utils/ConnectWallet.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+
+interface ConnectWalletProps {
+    address: string;
+}
+
+
+function ConnectWallet ({ address } : ConnectWalletProps ) {
+
+
+    return (
+
+    );
+}
