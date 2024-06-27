f47bb1df09ea880498cf7ce8fd7e30fdf2630522
Kevin Kamto <kamtokevin@gmail.com>
2024-06-27 06:39:07 -0400

    feat: implement basic wallet slice

---
+++ src/features/wallet/walletSlice.ts
@@ -0,0 +1,47 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { EMPTY_ADDRESS } from "../../utils/constant";
+
+interface WalletState {
+    address: string;
+    loading: boolean;
+    error: string;
+    isConnected: boolean;
+}
+
+const initialState: WalletState = {
+    address: EMPTY_ADDRESS,
+    loading: false,
+    error: "",
+    isConnected: false,
+}
+
+export const connectWallet = createAsyncThunk('wallet/connect', async () => {});
+
+export const disconnectWallet = createAsyncThunk('wallet/disconnect', async () => {});
+
+export const walletSlice = createSlice({
+    name: 'wallet',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(connectWallet.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.error.message!;
+        })
+        builder.addCase(connectWallet.pending, (state) => {
+            state.loading = true;
+            state.error = '';
+        })
+        builder.addCase(connectWallet.fulfilled, (state, action) => {
+            state.loading = false;
+            state.error = '';
+        })
+        builder.addCase(disconnectWallet.fulfilled, (state, action) => {
+            state.loading = false;
+            state.isConnected = false;
+            state.address = EMPTY_ADDRESS;
+        })
+    }
+});
+
+export default walletSlice.reducer;
