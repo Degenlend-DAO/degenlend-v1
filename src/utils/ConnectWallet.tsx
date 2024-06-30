4874d9576f231bae089fcef047c6e271b38cbc6d
Kevin Kamto <kamtokevin@gmail.com>
2024-06-29 22:19:47 -0400

    feat: connect & disconnect wallet

--- src/utils/ConnectWallet.tsx
+++ src/utils/ConnectWallet.tsx
@@ -1,6 +1,10 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import { useDispatch, useSelector } from 'react-redux';
+import { connectWallet, disconnectWallet } from '../features/wallet/walletSlice';
+import { AppDispatch, RootState } from "../app/Store";
+import { EMPTY_ADDRESS } from './constant';
 
 interface ConnectWalletProps {
     address: string;
@@ -8,17 +12,51 @@ interface ConnectWalletProps {
 }
 
 
-const ConnectWallet = ({ address, connectWallet }: ConnectWalletProps) => {
+const ConnectWallet = () => {
+
+    const isWalletConnected = useSelector((state: RootState) => state.wallet.isConnected);
+    const walletAddress = useSelector((state: RootState) => state.wallet.address);
+    const emptyAddress = EMPTY_ADDRESS;
+
+    const filteredWalletAddress = (address: string | undefined) => {
+        const size = address!.length;
+        const prefix = address!.substring(0, 5);
+        const suffix = address!.substring(size - 5);
+        return `${prefix}...${suffix}`;
+    }
+        // action items 
+
+    const dispatch = useDispatch<AppDispatch>();
+
+    const onConnectWallet = () => {
+        dispatch( connectWallet() );
+    }
+
+    const onDisconnectWallet = () => {
+        dispatch( disconnectWallet() );
+    }
+
     return (
         <Box sx={{ maxWidth: '200px' }}>
+        {
+            isWalletConnected ? 
             <Button
                 variant='contained'
-                onClick={connectWallet}
+                onClick={onDisconnectWallet}
+                size='medium'>
+                {filteredWalletAddress(walletAddress)}
+            </Button> 
+
+            :
+            <Button
+                variant='contained'
+                onClick={onConnectWallet}
                 size="medium">
                 Connect Wallet
             </Button>
-        </Box>
-    );
+        }
+            </Box>
+        );
 }
 
 export default ConnectWallet;
