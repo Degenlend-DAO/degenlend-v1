8bab1b39aa2991f1ae00111a20a9b192686c1b59
rbharath99 <dbossman60@gmail.com>
2024-07-08 11:32:31 -0400

    fix: runtime crash if a user does not have any wallet extension installed in browser

--- src/utils/web3.ts
+++ src/utils/web3.ts
@@ -1,28 +1,32 @@
-
-
-// This file is a collection of Web3 objects for the UI -- Starting with the wallet
 import Onboard from '@web3-onboard/core';
 import injectedModule from '@web3-onboard/injected-wallets';
-
-
-
-// Wallet utils
+import degenlendLogo from '../assets/img/degenlend-variation2.png'
 
 const injected = injectedModule();
 export const onboard = Onboard({
-    wallets: [injected],
-    chains: [
-        {
-            id: '0x287', // 647 in hex
-            token: 'SX',
-            label: 'SX Testnet',
-            rpcUrl: 'https://rpc.toronto.sx.technology/'
-          },
-          {
-            id: '0x1A0', // 416 in hex
-            token: 'SX',
-            label: 'SX Network',
-            rpcUrl: 'https://rpc.sx.technology/'
-          },
+  wallets: [injected],
+  chains: [
+    {
+      id: '0x287', // 647 in hex
+      token: 'SX',
+      label: 'SX Testnet',
+      rpcUrl: 'https://rpc.toronto.sx.technology/'
+    },
+    {
+      id: '0x1A0', // 416 in hex
+      token: 'SX',
+      label: 'SX Network',
+      rpcUrl: 'https://rpc.sx.technology/'
+    },
+  ],
+  appMetadata: {
+    name: 'DegenLend Protocol',
+    icon: degenlendLogo,
+    logo: degenlendLogo,
+    description: 'Swap tokens for other tokens',
+    recommendedInjectedWallets: [
+      { name: 'MetaMask', url: 'https://metamask.io' },
+      { name: 'Coinbase', url: 'https://wallet.coinbase.com/' }
     ]
+  },
 });
