b024f385ffdfac0ec7438150bce269098d155e02
rbharath99 <dbossman60@gmail.com>
2024-07-08 10:59:54 -0400

    style: top navigation bar connect wallet button and dark theme toggle

--- src/components/Header/TopNavigationBar.tsx
+++ src/components/Header/TopNavigationBar.tsx
@@ -1,35 +1,20 @@
-import React, { useState } from 'react';
-import { PaletteMode, useTheme } from '@mui/material';
+import React from 'react';
+import { useTheme } from '@mui/material';
 import Box from '@mui/material/Box';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
-import Divider from '@mui/material/Divider';
 import Typography from '@mui/material/Typography';
 import MenuItem from '@mui/material/MenuItem';
-import Drawer from '@mui/material/Drawer';
-import MenuIcon from '@mui/icons-material/Menu';
 import ToggleColorMode from '../../utils/ToggleColorMode'
 import { Link } from 'react-router-dom';
 
-import degenlendLogo from '../../assets/img/degenlend-variation2.png'; // interesting that this fixes the rendering issue
+import degenlendLogo from '../../assets/img/degenlend-variation2.png';
 import { ColorModeContext } from '../../utils/useMode';
 import ConnectWallet from '../../utils/ConnectWallet';
-
-
-// Action Items
-import { connectWallet, disconnectWallet } from '../../features/wallet/walletSlice';
-import { AppDispatch, RootState } from "../../app/Store";
-
-
- 
 
 const TopNavigationBar = () => {
     const theme = useTheme();
     const colorMode = React.useContext(ColorModeContext);
-
-
     return (
         <AppBar
             position='fixed'
@@ -40,66 +25,52 @@ const TopNavigationBar = () => {
                         : 'rgba(0, 0, 0, 1)',
             })}
         >
-            <Container maxWidth='lg'>
-                <Toolbar
+            <Toolbar
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                    flexShrink: 0,
+                    maxHeight: 40,
+                }}
+            >
+                <Box
+                    component="img"
                     sx={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'space-between',
-                        flexShrink: 0,
-                        maxHeight: 40,
+                        height: 40,
+                        width: 40,
                     }}
-                >
-                    <Box
-                        sx={{
-                            flexGrow: 1,
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'space-between',
-                            ml: '5px',
-                        }}
-                    >
-                        <Box
-                            component="img"
-                            sx={{
-                                height: 40,
-                                width: 40,
-                            }}
-                            alt="Degenlend Logo"
-                            src={degenlendLogo}
-                        />
-                        <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-                            {/* Supplying & Borrowing Activities on The market */}
-                            <MenuItem
-                            >
-                                <Typography variant="body2" color="text.primary">
-                                    <Link to="/markets">Markets</Link>
-                                </Typography>
-                            </MenuItem>
-                            <MenuItem>
-                                <Typography variant='body2' color='text.primary'>
-                                    <Link to="/governance">Governance</Link>
-                                </Typography>
-                            </MenuItem>
-                            {/* FAQ for how DegenLend works, and what to expect */}
-                            <MenuItem>
-                                <Typography variant="body2" color="text.primary">
-                                    <Link to="/faq">FAQ</Link>
-                                </Typography>
-                            </MenuItem>
-                            <MenuItem>
-                                <Typography variant='body2' color='text.primary'>
-                                    <Link to="/vote">Vote</Link>
-                                </Typography>
-                            </MenuItem>
-                        </Box>
-                        <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'end', flexGrow: 1 }}>
-                            <ToggleColorMode mode={theme.palette.mode} colorMode={colorMode} />
-                            <ConnectWallet />
-                        </Box>
-                    </Box>
-                </Toolbar>
-            </Container>
+                    alt="Degenlend Logo"
+                    src={degenlendLogo}
+                />
+                <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+                    <MenuItem>
+                        <Typography variant="body2" color="text.primary">
+                            <Link to="/markets">Markets</Link>
+                        </Typography>
+                    </MenuItem>
+                    <MenuItem>
+                        <Typography variant='body2' color='text.primary'>
+                            <Link to="/governance">Governance</Link>
+                        </Typography>
+                    </MenuItem>
+                    <MenuItem>
+                        <Typography variant="body2" color="text.primary">
+                            <Link to="/faq">FAQ</Link>
+                        </Typography>
+                    </MenuItem>
+                    <MenuItem>
+                        <Typography variant='body2' color='text.primary'>
+                            <Link to="/vote">Vote</Link>
+                        </Typography>
+                    </MenuItem>
+                </Box>
+                <Box sx={{ flexGrow: 1 }} />
+                <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'end' }}>
+                    <ToggleColorMode mode={theme.palette.mode} colorMode={colorMode} />
+                    <ConnectWallet />
+                </Box>
+            </Toolbar>
         </AppBar>
     )
 
