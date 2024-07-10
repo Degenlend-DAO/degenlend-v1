5e4c60d749c00a35fba455241f4098616eb91f1c
Kevin Kamto <kamtokevin@gmail.com>
2024-07-09 22:14:54 -0400

    chore: price oracle contract

--- src/utils/web3.ts
+++ src/utils/web3.ts
@@ -35,7 +35,7 @@ export const onboard = Onboard({
 // USDC, WSX, and Comptroller addresses
 export const testnet_addresses = {
   "comptroller": '0x8D1230e6Ae4C1Bc573697D93103349C3FDefC944',
-  "price_oracle": '',
+  "price_oracle": '0x6ca684b4773aF95AB5AE8d0aB7bA078237536DDF',
   "degenUSDC": '0xC863E82CD46296F1F81C63cDEB3708505B5b0d97',
   "degenWSX": '0x5cB7786A478eEc37Da5F6EA2e946cD860E784743',
 }
