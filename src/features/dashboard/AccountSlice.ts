66479e4e7b48a72994bae47be331a4f6c2ceddff
Kevin Kamto <kamtokevin@gmail.com>
2024-07-09 22:08:05 -0400

    feat: enter & exit markets thunks

--- src/features/dashboard/AccountSlice.ts
+++ src/features/dashboard/AccountSlice.ts
@@ -1,6 +1,9 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { onboard, testnet_addresses } from '../../utils/web3';
+import { ethers, Contract, formatUnits } from 'ethers'
 
-
+// ABIs
+import {abi} from '../../abis/Comptroller.json'
 interface AccountState {
     loading: boolean;
     error: string;
@@ -23,19 +26,59 @@ const initialState: AccountState = {
 
 // Views
 
-export const updateNetSupplyBalance = createAsyncThunk('netSupplyBalance/update', () => {})
+export const updateNetSupplyBalance = createAsyncThunk('netSupplyBalance/update', () => {
+    
+})
 
-export const updateNetBorrowBalance = createAsyncThunk('netBorrowBalance/update', () => {})
+export const updateNetBorrowBalance = createAsyncThunk('netBorrowBalance/update', () => {
 
-export const updateBorrowLimit = createAsyncThunk('borrowLimit/update', () => {})
+})
+
+export const updateBorrowLimit = createAsyncThunk('borrowLimit/update', () => {
+
+})
 
 export const updateNetAPR = createAsyncThunk('netAPR/update', () => {})
 
 // Activities
 
-export const enterMarket = createAsyncThunk('account/enterMarket', async () => {})
+export const enterMarket = createAsyncThunk('account/enterMarket', async () => {
+    const [wallet] = onboard.state.get().wallets;
+    let ethersProvider = new ethers.BrowserProvider(wallet.provider, 'any')
+    const signer = await ethersProvider.getSigner();
+    const comptroller = new Contract(testnet_addresses.comptroller, abi, signer);
 
-export const exitMarket = createAsyncThunk('account/exitMarket', async () => {})
+    let marketsToEnter = [testnet_addresses.degenWSX];
+
+    try {
+        let txn = await comptroller.enterMarkets(marketsToEnter);
+        await txn.wait();
+        console.log(txn);
+    } catch (error) {
+        console.log(`something went wrong: ${error}`)
+    }
+
+    console.log("Done: 'enterMarket' ");
+})
+
+export const exitMarket = createAsyncThunk('account/exitMarket', async () => {
+    const [wallet] = onboard.state.get().wallets;
+    let ethersProvider = new ethers.BrowserProvider(wallet.provider, 'any')
+    const signer = await ethersProvider.getSigner();
+    const comptroller = new Contract(testnet_addresses.comptroller, abi, signer);
+
+    let marketToExit = testnet_addresses.degenWSX;
+
+    try {
+        let txn = await comptroller.exitMarket(marketToExit);
+        await txn.wait();
+        console.log(txn);
+    } catch (error) {
+        console.log(`something went wrong: ${error}`)
+    }
+
+    console.log("Done: 'exitMarket' ");
+})
 
 export const AccountSlice = createSlice({
     name: "Account",
