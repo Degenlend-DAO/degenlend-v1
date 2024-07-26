f5f832fa2ef0cc0ce61bee35d88a52431c229f9f
Kevin Kamto <kamtokevin@gmail.com>
2024-07-25 21:53:05 -0400

    chore: latest working changes on tabs

--- src/components/Dashboard/usdcMarket.tsx
+++ src/components/Dashboard/usdcMarket.tsx
@@ -11,6 +11,10 @@ import DialogTitle from '@mui/material/DialogTitle';
 import Slide from '@mui/material/Slide';
 import { TransitionProps } from '@mui/material/transitions';
 
+// Token Information
+import usdcTokenLogo from '../../assets/img/usdc_coin_token.png'
+import { Box } from '@mui/material';
+
 
 const Transition = React.forwardRef(function Transition(
     props: TransitionProps & {
@@ -34,6 +38,36 @@ function USDCMarketDialog() {
     const handleClose = () => {
       setOpen(false);
     };
+
+    interface TabPanelProps {
+      children?: React.ReactNode;
+      index: number;
+      value: number;
+    }
+    
+    function CustomTabPanel(props: TabPanelProps) {
+      const { children, value, index, ...other } = props;
+    
+      return (
+        <div
+          role="tabpanel"
+          hidden={value !== index}
+          id={`simple-tabpanel-${index}`}
+          aria-labelledby={`simple-tab-${index}`}
+          {...other}
+        >
+          {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+        </div>
+      );
+    }
+
+    function a11yProps(index: number) {
+      return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+      };
+    }
+    
   
     return (
       <React.Fragment>
@@ -47,17 +81,18 @@ function USDCMarketDialog() {
           onClose={handleClose}
           aria-describedby="alert-dialog-slide-description"
         >
-          <DialogTitle>{"Use Google's location service?"}</DialogTitle>
+          <DialogTitle>
+<div style={{ textAlign: 'center'}}>
+<Box component="img" src={usdcTokenLogo} alt={"USDC Logo"} sx={{ height: 25, width: 30}}></Box>
+<a> USDC Token</a>
+</div>
+            </DialogTitle>
           <DialogContent>
             <DialogContentText id="alert-dialog-slide-description">
               Let Google help apps determine location. This means sending anonymous
               location data to Google, even when no apps are running.
             </DialogContentText>
           </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose}>Disagree</Button>
-            <Button onClick={handleClose}>Agree</Button>
-          </DialogActions>
         </Dialog>
       </React.Fragment>
     );
--- src/components/Dashboard/wsxMarket.tsx
+++ src/components/Dashboard/wsxMarket.tsx
@@ -11,6 +11,10 @@ import DialogTitle from '@mui/material/DialogTitle';
 import Slide from '@mui/material/Slide';
 import { TransitionProps } from '@mui/material/transitions';
 
+// Token Information
+import sxTokenLogo from '../../assets/img/sx_coin_token.png'
+import { Box, Tab, Tabs } from '@mui/material';
+
 
 const Transition = React.forwardRef(function Transition(
     props: TransitionProps & {
@@ -26,7 +30,7 @@ const Transition = React.forwardRef(function Transition(
 function WSXMarketDialog() {
 
     const [open, setOpen] = React.useState(false);
-
+    const [value, setValue] = React.useState(0);
     const handleClickOpen = () => {
       setOpen(true);
     };
@@ -34,6 +38,40 @@ function WSXMarketDialog() {
     const handleClose = () => {
       setOpen(false);
     };
+
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+      setValue(newValue);
+    };
+
+    interface TabPanelProps {
+      children?: React.ReactNode;
+      index: number;
+      value: number;
+    }
+    
+    function CustomTabPanel(props: TabPanelProps) {
+      const { children, value, index, ...other } = props;
+    
+      return (
+        <div
+          role="tabpanel"
+          hidden={value !== index}
+          id={`simple-tabpanel-${index}`}
+          aria-labelledby={`simple-tab-${index}`}
+          {...other}
+        >
+          {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+        </div>
+      );
+    }
+
+    function a11yProps(index: number) {
+      return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+      };
+    }
+    
   
     return (
       <React.Fragment>
@@ -47,17 +85,28 @@ function WSXMarketDialog() {
           onClose={handleClose}
           aria-describedby="alert-dialog-slide-description"
         >
-          <DialogTitle>{"Use Google's location service?"}</DialogTitle>
+          <DialogTitle>
+    <div style={{ textAlign: 'center'}}>
+    <Box component="img" src={sxTokenLogo} alt={"Wrapped SX Logo"} sx={{ height: 25, width: 30}}></Box>
+          <a> WSX Token</a></div>
+          </DialogTitle>
           <DialogContent>
-            <DialogContentText id="alert-dialog-slide-description">
+          <Box sx={{ borderBottom: 1, borderColor: 'divider', textAlign: 'center' }}>
+            <Tabs value={value} onChange={handleChange} aria-label="Supply & Borrow Tabs">
+              <Tab label="Supply" {...a11yProps(0)} />
+              <Tab label="Withdraw" {...a11yProps(1)} />
+            </Tabs>
+          </Box>
+          <CustomTabPanel value={value} index={0}>
+        Item One
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+      <DialogContentText id="alert-dialog-slide-description">
               Let Google help apps determine location. This means sending anonymous
               location data to Google, even when no apps are running.
-            </DialogContentText>
+            </DialogContentText>      </CustomTabPanel>
+
           </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose}>Disagree</Button>
-            <Button onClick={handleClose}>Agree</Button>
-          </DialogActions>
         </Dialog>
       </React.Fragment>
     );
