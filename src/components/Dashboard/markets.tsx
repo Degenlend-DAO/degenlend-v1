10eed716e050b5597cbc3261e8428239d0379858
Kevin Kamto <kamtokevin@gmail.com>
2024-07-29 16:24:00 -0400

    style: cleanup & styling improvements

--- src/components/Dashboard/markets.tsx
+++ src/components/Dashboard/markets.tsx
@@ -1,36 +1,44 @@
-import { Box, Button, Container, InputAdornment, Paper, Switch, TableContainer, TextField, Typography } from '@mui/material';
+import {
+  Container,
+} from "@mui/material";
 
-import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
+import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
 
-// Action Items
-import BorrowMarkets from './borrow_markets/borrowMarketsCard';
-import SupplyMarkets from './supply_markets/supplyMarketsCard';
-
+// Market Items
+import BorrowMarkets from "./borrow_markets/borrowMarketsCard";
+import SupplyMarkets from "./supply_markets/supplyMarketsCard";
 
 function Markets() {
+  //  This fragment holds the supply & borrow markets tables in one container
 
-    //  This react fragment holds the supply & borrow markets tables in one container
-
-    return (
-        <Container
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                pt: { xs: 14, sm: 20 },
-                // pb: { xs: 8, sm: 12 },
-            }} >
-            {/* <CustomBarChart /> */}
-            <Grid container spacing={6} columns={16} justifyContent="space-evenly" alignItems="stretch">
-            <Grid xs>
-                <SupplyMarkets />
-            </Grid>
-            <Grid xs>
-                <BorrowMarkets />
-            </Grid>
-            </Grid>
-        </Container>
-    )
+  return (
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        pt: { xs: 14, sm: 20 },
+        pb: { xs: 8, sm: 12 },
+      }}
+    >
+      {/* <CustomBarChart /> */}
+      <Grid
+        container
+        spacing={6}
+        columns={16}
+        justifyContent="space-evenly"
+        alignItems="stretch"
+        sx={{ width: '100%' }}
+      >
+        <Grid xs={8}>
+          <SupplyMarkets />
+        </Grid>
+        <Grid xs={8}>
+          <BorrowMarkets />
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
-export default Markets
+export default Markets;
