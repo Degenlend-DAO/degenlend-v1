17155d9c939b58d13a26ff0f454e9ddfa4e819f6
Kevin Kamto <kamtokevin@gmail.com>
2024-07-28 22:32:36 -0400

    refactor: first pass at styling markets page

--- src/components/Dashboard/markets.tsx
+++ src/components/Dashboard/markets.tsx
@@ -1,51 +1,15 @@
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
 import { Box, Button, Container, InputAdornment, Paper, Switch, TableContainer, TextField, Typography } from '@mui/material';
-import { Search } from '@mui/icons-material';
 
-// Token Logos
-import sxTokenLogo from '../../assets/img/sx_coin_token.png'
-import usdcTokenLogo from '../../assets/img/usdc_coin_token.png'
-import CustomBarChart from './MarketSupplyChart';
+import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 
 // Action Items
-import { useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, RootState } from '../../app/Store';
-import USDCMarketDialog from './supply_markets/usdcSupplyMarketDialog';
-import WSXMarketDialog from './supply_markets/wsxSupplyMarketDialog';
 import BorrowMarkets from './borrow_markets/borrowMarketsCard';
 import SupplyMarkets from './supply_markets/supplyMarketsCard';
 
 
 function Markets() {
 
-    const dispatch = useDispatch<AppDispatch>();
-    // Generate Markets Data
-
-    const usdcSupplyAPY = useSelector((state: RootState) => state.usdc.supplyRate);
-    const usdcBorrowAPY = useSelector((state: RootState) => state.usdc.borrowRate);;
-    const usdcWalletBalance = useSelector((state: RootState) => state.usdc.walletBalance);;
-    const usdcCollateral = useSelector((state: RootState) => state.usdc.isCollateral);
-    const usdcOraclePrice = useSelector((state: RootState) => state.usdc.oraclePrice);
-
-    const wsxSupplyAPY = useSelector((state: RootState) => state.wsx.supplyRate);
-    const wsxBorrowAPY = useSelector((state: RootState) => state.wsx.borrowRate);
-    const wsxWalletBalance = useSelector((state: RootState) => state.wsx.walletBalance);
-    const wsxCollateral = useSelector((state: RootState) => state.wsx.isCollateral);
-    const wsxOraclePrice = useSelector((state: RootState) => state.wsx.oraclePrice);
-
-    const toggleWSXCollateral = () => {
-        alert(`Toggling WSX from ${wsxCollateral} to ${!wsxCollateral}`)
-        
-    }
-
-    const toggleUSDCCollateral = () => {
-        alert(`Toggling USDC from ${usdcCollateral} to ${!usdcCollateral}`)
-
-    }
+    //  This react fragment holds the supply & borrow markets tables in one container
 
     return (
         <Container
@@ -54,11 +18,17 @@ function Markets() {
                 flexDirection: 'column',
                 alignItems: 'center',
                 pt: { xs: 14, sm: 20 },
-                pb: { xs: 8, sm: 12 },
+                // pb: { xs: 8, sm: 12 },
             }} >
             {/* <CustomBarChart /> */}
+            <Grid container spacing={6} columns={16}>
+            <Grid xs={8}>
             <SupplyMarkets />
-            <BorrowMarkets />
+  </Grid>
+  <Grid xs={8}>
+  <BorrowMarkets />
+  </Grid>
+            </Grid>
         </Container>
     )
 }
