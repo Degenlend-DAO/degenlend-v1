4e17c0ee65e461c4df8ddda25f0bf07a2fa05ecd
Kevin Kamto <kamtokevin@gmail.com>
2024-07-28 23:12:17 -0400

    refactor: enable markets

--- src/components/Dashboard/supply_markets/enableCollateralDialog.tsx
+++ src/components/Dashboard/supply_markets/enableCollateralDialog.tsx
@@ -1,7 +1,6 @@
 
 import * as React from 'react';
-import Button from '@mui/material/Button';
-import { Box, Tab, Tabs } from '@mui/material';
+import { Box, Switch, Tab, Tabs } from '@mui/material';
 
 
 // Dialogs
@@ -22,11 +21,14 @@ const Transition = React.forwardRef(function Transition(
     return <Slide direction="up" ref={ref} {...props} />;
   });
 
+  interface EnableMarketsProps {
+    
+  }
 
-function EnableMarketDialog() {
+function EnableMarketDialog(props: EnableMarketsProps) {
 
     const [open, setOpen] = React.useState(false);
-    const [value, setValue] = React.useState(0);
+    const [value, setValue] = React.useState(false);
     const handleClickOpen = () => {
       setOpen(true);
     };
@@ -35,45 +37,16 @@ function EnableMarketDialog() {
       setOpen(false);
     };
 
-    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    const handleChange = (event: React.SyntheticEvent, newValue: boolean) => {
       setValue(newValue);
     };
 
-    interface TabPanelProps {
-      children?: React.ReactNode;
-      index: number;
-      value: number;
-    }
-    
-    function CustomTabPanel(props: TabPanelProps) {
-      const { children, value, index, ...other } = props;
-    
-      return (
-        <div
-          role="tabpanel"
-          hidden={value !== index}
-          id={`simple-tabpanel-${index}`}
-          aria-labelledby={`simple-tab-${index}`}
-          {...other}
-        >
-          {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
-        </div>
-      );
-    }
-
-    function a11yProps(index: number) {
-      return {
-        id: `simple-tab-${index}`,
-        'aria-controls': `simple-tabpanel-${index}`,
-      };
-    }
     
   
     return (
       <React.Fragment>
-        <Button variant="outlined" onClick={handleClickOpen}>
-        Supply/Borrow
-        </Button>
+                            <Switch checked={open} onChange={handleChange} />
+
         <Dialog
           open={open}
           TransitionComponent={Transition}
@@ -84,23 +57,11 @@ function EnableMarketDialog() {
           <DialogTitle>
     <div style={{ textAlign: 'center'}}>
     <Box component="a" sx={{ height: 25, width: 30}}> Enable Collateral</Box>
-          <a> WSX Token</a></div>
+          </div>
           </DialogTitle>
           <DialogContent>
-          <Box sx={{ borderBottom: 1, borderColor: 'divider', textAlign: 'center' }}>
-            <Tabs value={value} onChange={handleChange} aria-label="Supply & Borrow Tabs">
-              <Tab label="Supply" {...a11yProps(0)} />
-              <Tab label="Withdraw" {...a11yProps(1)} />
-            </Tabs>
-          </Box>
-          <CustomTabPanel value={value} index={0}>
-        Item One
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={1}>
-      <DialogContentText id="alert-dialog-slide-description">
-              Let Google help apps determine location. This means sending anonymous
-              location data to Google, even when no apps are running.
-            </DialogContentText>      </CustomTabPanel>
+
+      
 
           </DialogContent>
         </Dialog>
@@ -108,3 +69,5 @@ function EnableMarketDialog() {
     );
 
 }
+
+export default EnableMarketDialog
