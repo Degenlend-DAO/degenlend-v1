6b48de7d7b10a7a82210fcbe75a2d859128fa8f0
Kevin Kamto <kamtokevin@gmail.com>
2024-07-31 07:14:46 -0400

    chore: the basics around repay

---
+++ src/components/Dashboard/supply_markets/widgets/repayDetails.tsx
@@ -0,0 +1,18 @@
+import { Box, Button, Stack, Divider, Typography } from "@mui/material";
+import BorrowRates from "./borrow/borrowRates";
+
+
+interface DetailProps {
+    type: String,
+}
+
+function RepayDetails(props: DetailProps) {
+
+    return (
+        <Box>
+            
+        </Box>
+    );
+}
+
+export default RepayDetails;
