2b563b071fb9a94a1d52110622fa7026cacc90e4
Kevin Kamto <kamtokevin@gmail.com>
2024-07-31 23:49:13 -0400

    withdraw details - first steps

---
+++ src/components/Dashboard/supply_markets/widgets/withdrawDetails.tsx
@@ -0,0 +1,18 @@
+import { Box, Button, Stack, Divider, Typography } from "@mui/material";
+import BorrowRates from "./borrow/borrowRates";
+
+
+interface DetailProps {
+    type: String,
+}
+
+function WithdrawDetails(props: DetailProps) {
+
+    return (
+        <Box>
+            
+        </Box>
+    );
+}
+
+export default WithdrawDetails;
