ec23109391acc476fee3c2cd1bb7557d820594f5
Kevin Kamto <kamtokevin@gmail.com>
2024-08-05 20:34:47 -0400

    chore: removed dead import

--- src/components/Dashboard/supply_markets/wsxSupplyMarketDialog.tsx
+++ src/components/Dashboard/supply_markets/wsxSupplyMarketDialog.tsx
@@ -6,7 +6,6 @@ import TabPanel from "@mui/lab/TabPanel";
 // Dialogs
 import Dialog from "@mui/material/Dialog";
 import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import Slide from "@mui/material/Slide";
 import { TransitionProps } from "@mui/material/transitions";
