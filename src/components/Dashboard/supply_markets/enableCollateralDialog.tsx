c32aa62ff358b402be08cab4e008074014444b63
Kevin Kamto <kamtokevin@gmail.com>
2024-08-19 05:24:39 -0400

    feat: fully functional enable & enter / exit markets!

--- src/components/Dashboard/supply_markets/enableCollateralDialog.tsx
+++ src/components/Dashboard/supply_markets/enableCollateralDialog.tsx
@@ -1,39 +1,52 @@
-
-import * as React from 'react';
-import { Box, Button, Divider, IconButton, Switch, Tab, Tabs, Typography } from '@mui/material';
-
+import * as React from "react";
+import {
+  Box,
+  Button,
+  Divider,
+  IconButton,
+  Switch,
+  Tab,
+  Tabs,
+  Typography,
+} from "@mui/material";
 
 // Dialogs
-import Dialog from '@mui/material/Dialog';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
 import CloseIcon from "@mui/icons-material/Close";
 
-import DialogTitle from '@mui/material/DialogTitle';
+import DialogTitle from "@mui/material/DialogTitle";
 
 // Action Items
 import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch, RootState } from '../../../app/Store';
+import { AppDispatch, RootState } from "../../../app/Store";
 
-import { enterUSDCMarket, enterWSXMarket, exitWSXMarket, exitUSDCMarket } from '../../../features/dashboard/AccountSlice';
-import { useState, useEffect } from 'react';
-import ConfirmTransactionDialog from '../widgets/confirmTransactionDialog';
-import { Transition } from '../../../utils/Transition';
+import {
+  enterUSDCMarket,
+  enterWSXMarket,
+  exitWSXMarket,
+  exitUSDCMarket,
+} from "../../../features/dashboard/AccountSlice";
+import { useState, useEffect } from "react";
+import ConfirmTransactionDialog from "../widgets/confirmTransactionDialog";
+import { Transition } from "../../../utils/Transition";
 
 interface EnableMarketsProps {
   open: boolean;
   onClose: () => void;
   title: string;
-  type: 'sx' | 'usdc';
+  type: "sx" | "usdc";
+  borrowLimit: number;
+  borrowLimitUsed: number;
 }
 
 function EnableMarketDialog(props: EnableMarketsProps) {
   const dispatch = useDispatch<AppDispatch>();
   const { onClose, type, open } = props;
 
-  let isCollateralTextHeader = `Enable ${props.title} as Collateral`;
-  let isCollateralTextBody = "";
-  let isCollateralTextButtonTitle = `Add ${props.title} as Collateral`;
+  const [isCollateralTextHeader, setIsCollateralTextHeader] = React.useState(`Enable ${props.title} as Collateral`);
+  const [isCollateralTextButtonTitle, setIsCollateralTextButtonTitle] = React.useState(`Add ${props.title} as Collateral`);
 
   const isUSDCCollateral = useSelector(
     (state: RootState) => state.usdc.isCollateral
@@ -45,8 +58,6 @@ function EnableMarketDialog(props: EnableMarketsProps) {
 
   const [confirmTransactionOpen, setConfirmTransactionOpen] = useState(false);
 
-
-
   const handleClick = () => {
     if (type === "sx") {
       // If wsx is already listed as collateral, exit the market
@@ -55,35 +66,38 @@ function EnableMarketDialog(props: EnableMarketsProps) {
       } else {
         dispatch(enterWSXMarket());
       }
-
     }
 
     if (type === "usdc") {
-
       // If USDC is already listed as collateral, exit the market
       if (isUSDCCollateral) {
         dispatch(exitUSDCMarket());
       } else {
+        dispatch(enterUSDCMarket());
       }
-      dispatch(enterUSDCMarket());
     }
     setConfirmTransactionOpen(true);
 
     onClose();
-  }
+  };
 
-  useEffect( () => {
-
-    if ( isUSDCCollateral === true || isWSXCollateral === true )
-    {
-      isCollateralTextHeader = `Remove ${props.title} as Collateral`;
-
-      isCollateralTextButtonTitle = `Remove ${props.title} as Collateral`;
-      
+  useEffect(() => {
+    if (type === "usdc") {
+      // If USDC is already listed as collateral, exit the market
+      if (isUSDCCollateral) {
+        setIsCollateralTextHeader(`Remove ${props.title} as Collateral`);
+        setIsCollateralTextButtonTitle(`Remove ${props.title} as Collateral`);
+      }
     }
 
+    if (type === "sx") {
+      // If wsx is already listed as collateral, exit the market
+      if (isWSXCollateral) {
+        setIsCollateralTextHeader(`Remove ${props.title} as Collateral`);
+        setIsCollateralTextButtonTitle(`Remove ${props.title} as Collateral`);
+      }
+    }
   });
-
 
   return (
     <React.Fragment>
@@ -97,8 +111,11 @@ function EnableMarketDialog(props: EnableMarketsProps) {
         aria-describedby="alert-dialog-slide-description"
       >
         <DialogTitle>
-          <div style={{ textAlign: 'center' }}>
-            <Box component="span" sx={{ fontSize: 20, fontWeight: 'bold' }}> {isCollateralTextHeader} </Box>
+          <div style={{ textAlign: "center" }}>
+            <Box component="span" sx={{ fontSize: 20, fontWeight: "bold" }}>
+              {" "}
+              {isCollateralTextHeader}{" "}
+            </Box>
           </div>
           <IconButton
             aria-label="close"
@@ -115,23 +132,41 @@ function EnableMarketDialog(props: EnableMarketsProps) {
           <Divider></Divider>
         </DialogTitle>
         <DialogContent>
-
           {/* Warning Label */}
-          <Box sx={{ textAlign: 'center', marginBottom: 2 }}>
-            <DialogContentText sx={{ color: 'text.secondary' }}>
-              Each asset used as collateral increases your borrowing limit. Be careful, this can subject the asset to being seized in liquidation.{' '}
-              <a href="#learn-more" style={{ color: 'inherit', textDecoration: 'underline' }}>
+          <Box sx={{ textAlign: "center", marginBottom: 2 }}>
+            <DialogContentText sx={{ color: "text.secondary" }}>
+              Each asset used as collateral increases your borrowing limit. Be
+              careful, this can subject the asset to being seized in
+              liquidation.{" "}
+              <a
+                href="#learn-more"
+                style={{ color: "inherit", textDecoration: "underline" }}
+              >
                 Learn more
-              </a>.
+              </a>
+              .
             </DialogContentText>
           </Box>
 
           {/* Borrow Limit Content */}
-          <Box sx={{ display: 'flex', justifyContent: 'space-between', marginTop: 2, marginBottom: 2 }}>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              marginTop: 2,
+              marginBottom: 2,
+            }}
+          >
             <Typography variant="body2">Borrow Limit</Typography>
             <Typography variant="body2">$0.00 → $0</Typography>
           </Box>
-          <Box sx={{ display: 'flex', justifyContent: 'space-between', marginBottom: 2 }}>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              marginBottom: 2,
+            }}
+          >
             <Typography variant="body2">Borrow Limit Used</Typography>
             <Typography variant="body2">0% → 0%</Typography>
           </Box>
@@ -141,23 +176,25 @@ function EnableMarketDialog(props: EnableMarketsProps) {
             variant="contained"
             color="primary"
             fullWidth
-            sx={{ marginTop: 2, fontWeight: 'bold', paddingY: 1.5 }}
-            onClick={handleClick} // Clicking the button 
+            sx={{ marginTop: 2, fontWeight: "bold", paddingY: 1.5 }}
+            onClick={handleClick} // Clicking the button
             aria-label="button to toggle collateral"
           >
             {isCollateralTextButtonTitle}
           </Button>
-
         </DialogContent>
       </Dialog>
 
-      <ConfirmTransactionDialog open={confirmTransactionOpen} onClose={() => { setConfirmTransactionOpen(false) }} />
-
+      <ConfirmTransactionDialog
+        open={confirmTransactionOpen}
+        onClose={() => {
+          setConfirmTransactionOpen(false);
+        }}
+      />
     </React.Fragment>
   );
-
 }
 
-export default EnableMarketDialog
+export default EnableMarketDialog;
 
-// TODO: 
+// TODO:
