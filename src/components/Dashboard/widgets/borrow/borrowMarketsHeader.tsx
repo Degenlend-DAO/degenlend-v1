3842fd72253d8d668eaf89a590c95bb1ff245c9e
Kevin Kamto <kamtokevin@gmail.com>
2024-08-26 08:01:33 -0400

    feat: create header files

---
+++ src/components/Dashboard/widgets/borrow/borrowMarketsHeader.tsx
@@ -0,0 +1,39 @@
+import {
+    Box,
+    Stack,
+    Typography,
+} from "@mui/material";
+
+import { useSelector } from "react-redux"
+import { RootState } from "../../../../app/Store";
+import EnableWarning from "../enableWarning";
+import { useEffect } from "react";
+import NumberInput from "../numberinput";
+
+interface BorrowMarketsHeaderProps {
+    type: string;
+    input?: boolean | undefined;
+}
+
+function BorrowMarketsHeader(props: BorrowMarketsHeaderProps) {
+
+    const {type, input } = props;
+    let BorrowMarkets:JSX.Element = <Box></Box>;
+    // useEffect();
+
+    if (input === undefined || input === false) {
+        BorrowMarkets = <EnableWarning type={type} />
+    }
+
+    if (input === true) {
+        BorrowMarkets = <NumberInput type={type} />
+    }
+
+    return (
+        <Box>
+            {BorrowMarkets}
+        </Box>
+    );
+}
+
+export default BorrowMarketsHeader;
---
+++ src/components/Dashboard/widgets/supply/supplyMarketsHeader.tsx
@@ -0,0 +1,43 @@
+import {
+  Box,
+  FormControl,
+  Input,
+  InputAdornment,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { getTokenDetails } from "../../../../utils/TokenType";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../../app/Store";
+import EnableWarning from "../enableWarning";
+import NumberInput from "../numberinput";
+
+
+interface SupplyMarketsHeaderProps {
+  type: string;
+  input: boolean;
+}
+
+function SupplyMarketsHeader(props: SupplyMarketsHeaderProps) {
+
+  const { type, input } = props;
+
+  let SupplyMarkets:JSX.Element = <Box></Box>;
+
+
+  if (input === undefined || input === false) {
+    SupplyMarkets = <EnableWarning type={type} />
+}
+
+if (input === true) {
+    SupplyMarkets = <NumberInput type={type} />
+}
+
+  return (
+    <Box>
+      {SupplyMarkets}
+    </Box>
+  );
+}
+
+export default SupplyMarketsHeader;
