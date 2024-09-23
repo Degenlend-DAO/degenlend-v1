43f48c0054d504b7821d817ccbed2fa450f217a8
Kevin Kamto <kamtokevin@gmail.com>
2024-09-23 04:23:00 -0400

    feat: made formanNumber public

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,1 +1,5 @@
 export const EMPTY_ADDRESS:string = "0x0000000000000000000000000000000000000000";
+
+export const formatNumber = (number: number, decimals = 6) => {
+    return number.toLocaleString(undefined, { minimumFractionDigits: decimals, maximumFractionDigits: decimals });
+  };
