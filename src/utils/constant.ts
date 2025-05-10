5e980e4a645f9c7fcb3ea59d9dfc1f50dd9a83b7
Kevin Kamto <kamtokevin@gmail.com>
2025-05-10 17:33:46 -0400

    chore: flipped testnet address

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,5 +1,5 @@
 export const EMPTY_ADDRESS:string = "0x0000000000000000000000000000000000000000";
-export const API_URL = "http://localhost:3001";
+export const API_URL = "http://localhost:3000";
 export const ORACLE_URL = "https://render.com/";
 export const DEGEN_TOKENS_DECIMALS = 8;
 
