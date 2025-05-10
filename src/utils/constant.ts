0b5cefb524efef5d65e7c8abc0f73065a378a16a
Kevin Kamto <kamtokevin@gmail.com>
2025-05-09 21:20:52 -0400

    chore: added standardized token decimals for DEGEN tokens (aka cTokens)

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,6 +1,7 @@
 export const EMPTY_ADDRESS:string = "0x0000000000000000000000000000000000000000";
 export const API_URL = "http://localhost:3001";
 export const ORACLE_URL = "https://render.com/";
+export const DEGEN_TOKENS_DECIMALS = 8;
 
 export const formatNumber = (number: number, decimals = 2) => {
     return number.toLocaleString(undefined, { minimumFractionDigits: decimals, maximumFractionDigits: decimals });
