b5fce6e9b0f5b502b4b5fa225922e2b17f106990
Kevin Kamto <kamtokevin@gmail.com>
2025-05-12 12:10:36 -0400

    feat: added prod-level degenlend API url

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,5 +1,6 @@
 export const EMPTY_ADDRESS:string = "0x0000000000000000000000000000000000000000";
 export const API_URL = "http://localhost:3000";
+export const LIVE_API_URL="https://degenlend-api.onrender.com/"
 export const ORACLE_URL = "https://render.com/";
 export const DEGEN_TOKENS_DECIMALS = 8;
 
