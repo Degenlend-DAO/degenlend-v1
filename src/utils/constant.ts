0fb76fd102fd67bc1ffe42847758fcf02328654f
Kevin Kamto <kamtokevin@gmail.com>
2025-05-12 14:27:41 -0400

    feat: switched to a deployed API URL

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,5 +1,5 @@
 export const EMPTY_ADDRESS:string = "0x0000000000000000000000000000000000000000";
-export const API_URL = "http://localhost:3000";
+export const API_URL = "https://degenlend-api-production.up.railway.app/";
 export const LIVE_API_URL="https://degenlend-api.onrender.com/"
 export const ORACLE_URL = "https://render.com/";
 export const DEGEN_TOKENS_DECIMALS = 8;
