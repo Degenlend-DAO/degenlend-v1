e7cb5b74133823a7b573047db869427c4dcf77ee
Kevin Kamto <kamtokevin@gmail.com>
2025-05-13 04:00:56 -0400

    feat: added sportsfi-oracle

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,7 +1,7 @@
 export const EMPTY_ADDRESS:string = "0x0000000000000000000000000000000000000000";
 export const API_URL = "https://degenlend-api-production.up.railway.app/";
 export const LIVE_API_URL="https://degenlend-api.onrender.com/"
-export const ORACLE_URL = "https://render.com/";
+export const ORACLE_URL = "https://sports-oracle-api.onrender.com/";
 export const DEGEN_TOKENS_DECIMALS = 8;
 
 export const formatNumber = (number: number, decimals = 2) => {
