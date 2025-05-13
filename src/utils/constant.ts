2d6a77f54098892e396d0b541ab5ecabc9e9e2c0
Kevin Kamto <kamtokevin@gmail.com>
2025-05-13 05:21:47 -0400

    fix: had an extra '/' that was messing things up

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,6 +1,6 @@
 export const EMPTY_ADDRESS:string = "0x0000000000000000000000000000000000000000";
-export const API_URL = "https://degenlend-api-production.up.railway.app/";
-export const LIVE_API_URL="https://degenlend-api.onrender.com/"
+export const API_URL = "https://degenlend-api-production.up.railway.app";
+export const LIVE_API_URL="https://degenlend-api.onrender.com"
 export const ORACLE_URL = "https://sports-oracle-api.onrender.com/";
 export const DEGEN_TOKENS_DECIMALS = 8;
 
