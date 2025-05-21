d19f4a7e571de011d3689d4958523eedf00f835b
Kevin Kamto <kamtokevin@gmail.com>
2025-05-21 17:11:48 -0400

    feat: shipped fresh contracts

--- src/utils/web3.ts
+++ src/utils/web3.ts
@@ -34,13 +34,13 @@ export const onboard = Onboard({
 
 // USDC, WSX, and Comptroller addresses
 export const testnet_addresses = {
-  "comptroller": '0x937C076C8D9F85291F5cd22E95b1212C0138A609',
+  "comptroller": '0xB078459124e55Eb9F2937c86c0Ec893ff4FF082b',
   "price_oracle": '0x6ca684b4773aF95AB5AE8d0aB7bA078237536DDF',
   "USDC": '0x5147891461a7C81075950f8eE6384e019e39ab98',
   "WSX": '0x2D4e10Ee64CCF407C7F765B363348f7F62D2E06e',
   "degenlendRelayer": "0x7d3Aef7925B0ad621032F87e42b2Af12e5639F76",
-  "degenWSX": '0x77273a5B3779e339B990e4BACf0222fc04e93198',
-  "degenUSDC": '0xC8A9b9B44B7DC55351706d94Cd986B053C5f2EaD', 
+  "degenWSX": '0x986208977A741f86D16Bda232a321F3d38E916Fc',
+  "degenUSDC": '0x35f1263CE6c06128c4075A1a82050CEB09fa1eB3', 
 }
 
 export const mainnet_addresses = {
